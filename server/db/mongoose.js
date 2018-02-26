0d3da46b6e33185c670f9dd50e1bb5433f623dd9
JotaPe <hernolas@gmail.com>
2018-02-26 22:35:34 +0000

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 
-//Condition to validate if app is in a PRD system or in localhost
-var connectionHost = process.env.PORT ? 'mongodb://dbUser:123456789@ds249128.mlab.com:49128/todo-app-api' : 'mongodb://localhost:27017/TodoApp';
-
 mongoose.Promise = global.Promise;
-mongoose.connect(connectionHost);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
