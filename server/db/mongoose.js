d59ab1c167f2d4d07eafd71d59d417ca1ee9335f
JotaPe <hernolas@gmail.com>
2018-02-26 20:43:04 +0000

    Add condition to validate if in localhost or in PRD environment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
+//Condition to validate if app is in a PRD system or in localhost
+var connectionHost = process.env.PORT ? 'mongodb://dbUser:123456789@ds249128.mlab.com:49128/todo-app-api' : 'mongodb://localhost:27017/TodoApp';
+
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://dbUser:123456789@ds249128.mlab.com:49128/todo-app-api');
-mongoose.connect('mongodb://dbUser:123456789@ds249128.mlab.com:49128/todo-app-api');
+mongoose.connect(connectionHost);
 
 module.exports = {mongoose};
